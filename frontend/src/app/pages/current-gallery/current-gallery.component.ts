4cc41716623e49d58fe75cd406363d34f72046f8
Sahar Guebsi <saharguebsi0@gmail.com>
2024-12-18 17:55:14 +0100

    final version

--- frontend/src/app/pages/current-gallery/current-gallery.component.ts
+++ frontend/src/app/pages/current-gallery/current-gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { Chart } from 'chart.js';
+
+import AOS from 'aos'; 
 
 @Component({
   selector: 'app-current-gallery',
@@ -9,38 +10,15 @@ import { Chart } from 'chart.js';
   templateUrl: './current-gallery.component.html',
   styleUrl: './current-gallery.component.css'
 })
-export class CurrentGalleryComponent implements OnInit {
-  chart: any;
 
-  constructor() { }
+export class CurrentGalleryComponent implements OnInit {
 
   ngOnInit(): void {
-    this.createChart();
-  }
-
-  createChart(): void {
-    const ctx = document.getElementById('attendanceChart') as HTMLCanvasElement;
-    
-    this.chart = new Chart(ctx, {
-      type: 'bar', // You can change this to 'line' or 'pie' based on your preference
-      data: {
-        labels: ['John', 'Jane', 'Jack', 'Jill'], // This would be dynamically populated with attendee names
-        datasets: [{
-          label: 'Feedback Rating (out of 5)',
-          data: [4, 5, 3, 4], // Feedback ratings
-          backgroundColor: 'rgba(99, 132, 255, 0.6)', // Color for bars
-          borderColor: 'rgba(99, 132, 255, 1)',
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
+    // Initialisation de AOS avec les options
+    AOS.init({
+      offset: 400,   // Décalage pour le déclenchement des animations
+      duration: 600, // Durée des animations en millisecondes
+      once: true,    // Animation jouée une seule fois (optionnel)
     });
   }
-
 }
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import AOS from 'aos';    
+
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -7,6 +11,13 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+ ngOnInit(): void {
+    // Initialisation de AOS avec les options
+    AOS.init({
+      offset: 400,   // Décalage pour le déclenchement des animations
+      duration: 600, // Durée des animations en millisecondes
+      once: true,    // Animation jouée une seule fois (optionnel)
+    });
+  }
 }
